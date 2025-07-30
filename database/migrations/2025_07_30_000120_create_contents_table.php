01f30d99f00141adc499b963eb24dff642232d13
ashik <ashikurrahman7134@gmail.com>
2025-07-30 07:11:25 +0600

    Content table database schema ready

---
+++ database/migrations/2025_07_30_000120_create_contents_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Module;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Module::class)->constrained()->onDelete('cascade');
+            $table->text('text')->nullable();
+            $table->string('image')->nullable();
+            $table->string('video')->nullable();
+            $table->string('link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contents');
+    }
+};
