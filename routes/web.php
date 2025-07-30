d6fe5f8315c5793e8c46b54318ddf87f65f3d853
ashik <ashikurrahman7134@gmail.com>
2025-07-30 11:08:31 +0600

    courses route added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CourseController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,4 @@ Route::get('/', function () {
 
 
 Route::resource('categories', CategoryController::class);
+Route::resource('courses', CourseController::class);
