34eb597dd2445e61dd0a5be7773f0fe7510674a3
ashik <ashikurrahman7134@gmail.com>
2025-07-30 07:15:04 +0600

    fillable property and relation with module

--- app/Models/Content.php
+++ app/Models/Content.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Content extends Model
 {
     protected $fillable = ['module_id', 'text', 'image', 'video', 'link'];
+
+    public function module()
+    {
+        return $this->belongsTo(Module::class);
+    }
 }
