c0097bd4f3c19fb94b74f839b1341fc4fb7936a5
ashik <ashikurrahman7134@gmail.com>
2025-07-30 07:10:59 +0600

    Course table database schema ready

---
+++ database/migrations/2025_07_30_000041_create_courses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Category;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Category::class)->constrained()->onDelete('cascade');
+            $table->string('title')->unique();
+            $table->string('slug');
+            $table->string('batch');
+            $table->decimal('price', 10, 2);
+            $table->text('description');
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
